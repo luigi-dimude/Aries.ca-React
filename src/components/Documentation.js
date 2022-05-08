af5bd153c24c237b612453de98b622ec016b5e51
luigi ekene <ekenedimude@gmail.com>
2022-05-07 22:43:35 -0400

    added documentation route

--- src/components/Documentation.js
+++ src/components/Documentation.js
@@ -1,8 +1,6 @@
 import React, {useEffect} from 'react'
-import { useNavigate } from 'react-router-dom'
 
 export const Documentation = () => {
-    let navigate = useNavigate();
 
     // On Page load
    useEffect( () => {
